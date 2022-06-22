310224b9bac7131ebd5be6effc2d05b0dd049e14
Alexandra Gallipoli Rodrigues <cxb0507@mbpdeitseradmin.hi.inet>
2022-06-22 09:28:30 +0200

    new functional algorithm implemented

--- src/main/kotlin/agallipo/main.kt
+++ src/main/kotlin/agallipo/main.kt
@@ -4,9 +4,9 @@ package agallipo
 fun main(args: Array<String>) {
     val pathname = "/Users/cxb0507/Desktop/example.json"
     var boardingCardJsonParser = BoardingCardJsonParser(pathname)
-    var myVar = boardingCardJsonParser.sortTickets(boardingCardJsonParser.boardingCards)
-    for (my in myVar){
-        println("MY:" + my)
+    var flightPaths = boardingCardJsonParser.sortTickets(boardingCardJsonParser.boardingCards)
+    for (flightPath in flightPaths){
+        println("MY:" + flightPath)
     }
 
 }
