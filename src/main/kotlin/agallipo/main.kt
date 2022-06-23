f85c03414927a6816fec8549c85b116894233185
Alexandra Gallipoli Rodrigues <cxb0507@mbpdeitseradmin.hi.inet>
2022-06-23 11:29:34 +0200

    tabulating all files for pr and adding gitignore

--- src/main/kotlin/agallipo/main.kt
+++ src/main/kotlin/agallipo/main.kt
@@ -1,12 +1,12 @@
-package agallipo
+    package agallipo
 
-//created by alex :)
-fun main(args: Array<String>) {
-    val pathname = "/Users/cxb0507/Desktop/example.json"
-    var boardingCardJsonParser = BoardingCardJsonParser(pathname)
-    var flightPaths = boardingCardJsonParser.sortTickets(boardingCardJsonParser.boardingCards)
-    for (flightPath in flightPaths){
-        println("MY:" + flightPath)
+    //created by alex :)
+    fun main(args: Array<String>) {
+        val pathname = "examples/example.json"
+        var boardingCardJsonParser = BoardingCardJsonParser(pathname)
+        var flightPaths = boardingCardJsonParser.sortTickets(boardingCardJsonParser.boardingCards)
+        for (flightPath in flightPaths){
+            println("MY:" + flightPath)
+        }
+
     }
-
-}
